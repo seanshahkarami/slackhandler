3d693741857647e46376e4efa36f774445c62cea
Sean Shahkarami <sean.shahkarami@gmail.com>
2017-03-02 22:47:23 -0600

    Added contact info.

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='slackhandler',
     version='0.1.0',
     description='Posts logs to Slack webhooks.',
+    author='Sean Shahkarami',
+    author_email='sean.shahkarami@gmail.com',
+    url='https://github.com/seanshahkarami/slackhandler',
     install_requires=['requests'],
     packages=['slackhandler'],
 )
