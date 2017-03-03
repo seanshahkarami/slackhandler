a2aa8500677979d020b0daec1b6c238817554653
Sean Shahkarami <sean.shahkarami@gmail.com>
2017-03-02 21:59:01 -0600

    First commit.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='slackhandler',
+    version='0.1.0',
+    description='Slack logging handler',
+    install_requires=['requests'],
+    packages=['slackhandler'],
+)
---
+++ slackhandler/__init__.py
@@ -0,0 +1,41 @@
+import logging
+import requests
+import json
+
+
+class SlackHandler(logging.Handler):
+
+    colors = {
+        'ERROR': '#e01765',
+        'WARNING': '#e8a723',
+        'INFO': '#3eb890',
+        'DEBUG': '#70cadb',
+    }
+
+    def __init__(self, webhook, **kwargs):
+        logging.Handler.__init__(self)
+        self.webhook = webhook
+        self.url = 'https://hooks.slack.com/services/{}'.format(webhook)
+
+    def emit(self, record):
+        params = record.__dict__
+        timestamp = params['created']
+        name = params['name']
+        level = params['levelname']
+        msg = params['msg']
+
+        color = self.colors.get(level, '#000000')
+
+        data = {
+            'attachments': [
+                {
+                    'ts': timestamp,
+                    'text': msg,
+                    'color': color,
+                    'author_name': name,
+                    'fallback': '{} {}'.format(level, msg),
+                }
+            ]
+        }
+
+        requests.post(self.url, data=json.dumps(data))
